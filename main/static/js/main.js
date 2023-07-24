938ca70b48716f1e9d4eb6d891996b8cee431fd0
songeunseon <ses920525@gmail.com>
2023-07-24 09:44:17 +0900

    송은선

--- main/static/js/main.js
+++ main/static/js/main.js
@@ -2,11 +2,26 @@ window.onload=function(){
     var pre=document.getElementsByClassName("previous")[0];
     var next=document.getElementsByClassName("next")[0];
     pre.addEventListener("click",function(){
-        var slideview=document.getElementById("slide")[0];
-        slideview.style.transform="translate3d(0px, 0px, 0px) ";
+        var slideview=document.getElementsByClassName("slide")[0];
+        var now_left=slideview.style.left.split("px")[0];
+        if(now_left == 0) return;
+        var left=Number(now_left)+400;
+        slideview.style.left=left+"px";
     });
     next.addEventListener("click",function(){
-        var slideview=document.getElementById("slide")[0];
-        slideview.style.transform="translate3d(-1950px, 0px, 0px) ";
+        var slideview=document.getElementsByClassName("slide")[0];
+        var now_left=slideview.style.left.split("px")[0];
+        if(now_left == -1010) return;
+        if(now_left== -860)
+        {
+            var left=now_left-150;
+            slideview.style.left=left+"px";
+        }else{
+        var left=now_left-430;
+        slideview.style.left=left+"px";
+        }
     });
+}
+function menu() {
+    window.location.href = "http://127.0.0.1:5500/main/item/toy_list.html";
 }
