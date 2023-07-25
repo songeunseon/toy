e4c94b8e291f4aae7fee4468300219c7c7c8078f
songeunseon <ses920525@gmail.com>
2023-07-25 12:32:14 +0900

    송은선

--- main/static/js/history.js
+++ main/static/js/history.js
@@ -1,11 +1,9 @@
-
-
 function menu() {
-    window.location.href = "http://127.0.0.1:5500/main/item/toy_list.html";
+    window.location.href = "item/toy_list.html";
 }
 function history() {
-    window.location.href = "http://127.0.0.1:5500/main/item/history.html";
+    window.location.href = "item/history.html";
 }
 function notice() {
-    window.location.href = "http://127.0.0.1:5500/main/item/event.html";
+    window.location.href = "item/event.html";
 }
